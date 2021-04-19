f346a5f364e547307a2270ee1f1f522a84186eb6
zhanghao731 <986136244@qq.com>
2021-04-19 14:10:31 +0800

    multilayer perceptron

---
+++ chap3.10.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+from torch.nn import init
+import numpy as np
+import d2lzh_pytorch as d2l
+
+num_inputs, num_outputs, num_hiddens = 784, 10, 256
+net = nn.Sequential(d2l.FlattenLayer(), nn.Linear(num_inputs, num_hiddens), nn.ReLU(),
+                    nn.Linear(num_hiddens, num_outputs), )
+for params in net.parameters():
+    init.normal_(params, mean = 0, std = 0.01)
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+loss = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr = 0.5)
+num_epoches = 5
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epoches, batch_size, None, None, optimizer)
---
+++ chap3.8.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import d2lzh_pytorch as d2l
+import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
+def xyplot(x_vals, y_vals, name):
+    d2l.set_figsize(figsize = (5, 2.5))
+    d2l.plt.plot(x_vals.detach().numpy(), y_vals.detach().numpy())
+    d2l.plt.xlabel('x')
+    d2l.plt.ylabel(name + '(x)')
+    d2l.plt.show()
+
+
+x = torch.arange(-8.0, 8.0, 0.1, requires_grad = True)
+y = x.relu()
+# xyplot(x, y, 'relu')
+y.sum().backward()
+# xyplot(x, x.grad, 'grad of relu')
+y = x.sigmoid()
+# xyplot(x, y, 'sigmoid')
+x.grad.zero_()
+y.sum().backward()
+# xyplot(x, x.grad, 'grad of sigmoid')
+y = x.tanh()
+# xyplot(x,y,'tanh')
+x.grad.zero_()
+y.sum().backward()
+xyplot(x, x.grad, 'grad of tanh')
