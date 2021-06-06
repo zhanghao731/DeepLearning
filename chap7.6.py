503f16af0ad56ca34239afe2efc0f69133b3324a
zhanghao731 <986136244@qq.com>
2021-06-06 16:01:50 +0800

    RMSProp算法

---
+++ chap7.6.py
@@ -0,0 +1,23 @@
+import torch
+
+import d2lzh_pytorch as d2l
+
+features, labels = d2l.get_data_ch7()
+
+
+def init_rmsprop_states():
+    s_w = torch.zeros((features.shape[1], 1), dtype = torch.float32)
+    s_b = torch.zeros(1, dtype = torch.float32)
+    return (s_w, s_b)
+
+
+def rmsprop(params, states, hyperparams):
+    gamma, eps = hyperparams['gamma'], 1e-6
+    for p, s in zip(params, states):
+        s.data = gamma * s.data + (1 - gamma) * (p.grad.data) ** 2
+        p.data -= hyperparams['lr'] * p.grad.data / torch.sqrt(s + eps)
+
+# d2l.train_ch7(rmsprop, init_rmsprop_states(), {'lr': 0.01, 'gamma': 0.9},
+#               features, labels)
+d2l.train_pytorch_ch7(torch.optim.RMSprop, {'lr': 0.01, 'alpha': 0.9},
+                    features, labels)
